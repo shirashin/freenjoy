2714037fb7e82330daca0b8530db0cd0d448f810
shirashin <mitsuhiro.seike@shirashin.com>
2016-12-10 23:27:50 +0900

    install annotate

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry-rails'
   gem 'rails-erd'
+  gem 'annotate'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  kind       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
 end
